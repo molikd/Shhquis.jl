da3b6f78e23b6e7bd966c1056fe300353ceec655
molikd <dave@d3fy.net>
2021-07-29 11:07:49 -1000

    added some files to gitignore
    new shh dup function has hclust linkage types now
    
    shh.jl now can take a hclust linkage type
    
    don't know how I didn't see that hclust_algo was a different variable
    
    added more to gitignore, will remove some of these later

--- bin/shh.jl
+++ bin/shh.jl
@@ -31,6 +31,10 @@ function parse_commandline()
             help = "the info_contigs.txt file"
             arg_type = String
             required = true
+        "--hclust-linkage"
+            help = "linkage is one of single, average, complete, ward, ward_presquared"
+            arg_type = String
+            required = false
     end
 
     return parse_args(s)
@@ -42,12 +46,46 @@ function main()
     for (arg,val) in parsed_args
         println("set  $arg  =>  $val")
     end
-    
-    shh(genomeoutfile=parsed_args["reorient"],
-                genomeinfile=parsed_args["genome"],
-                genomefaifile=parsed_args["fai"],
-                bg2file=parsed_args["bg2"],
-                contiginfofile=parsed_args["contig"]) 
+
+    reorient = parsed_args["reorient"]
+    genome = parsed_args["genome"]
+    fai = parsed_args["fai"]
+    bg2 = parsed_args["bg2"]
+    contig = parsed_args["contig"]
+
+
+    if parsed_args["hclust-linkage"] != nothing
+        hclust_algo = parsed_args["hclust-linkage"]
+
+        if hclust_algo == "single"
+            hclust_sym = :single
+        elseif hclust_algo == "average"
+            hclust_sym = :average
+        elseif hclust_algo == "complete"
+            hclust_sym = :complete
+        elseif hclust_algo == "ward"
+            hclust_sym = :ward
+        elseif hclust_algo == "ward_presquared"
+            hclust_sym = :ward_presquared
+        else
+            error("not a real hclust linkage type")
+            exit(code=1)
+        end
+
+        shh(genomeoutfile=parsed_args["reorient"],
+            genomeinfile=parsed_args["genome"],
+            genomefaifile=parsed_args["fai"],
+            bg2file=parsed_args["bg2"],
+            contiginfofile=parsed_args["contig"],
+            hclust_linkage=hclust_sym)
+    else
+        shh(genomeoutfile=parsed_args["reorient"],
+            genomeinfile=parsed_args["genome"],
+            genomefaifile=parsed_args["fai"],
+            bg2file=parsed_args["bg2"],
+            contiginfofile=parsed_args["contig"])
+    end
+
 end
 
 main()
--- src/shhquis.jl
+++ src/shhquis.jl
@@ -20,7 +20,24 @@ function shh(;genomeoutfile::AbstractString="genome.reoriented.fasta",
    names = @view contiginfo[1,][:,1]
 
    dist = builddist(contiginfofile,bg2file)
-   clustres = hclust(dist)
+   clustres = hclust(dist, branchorder=:optimal)
+   neworder = ordernames(clustres.order,names)
+   frinfo = reorient(contiginfofile,bg2file,neworder)
+   write_reorient(genomeinfile,genomeoutfile,frinfo,genomefaifile)  
+
+end
+
+function shh(;genomeoutfile::AbstractString="genome.reoriented.fasta",
+                    genomeinfile::AbstractString="genome.fasta",
+                    genomefaifile::AbstractString="genome.fasta.fai",
+                    bg2file::AbstractString="abs_fragments_contacts_weighted.bg2",
+                    contiginfofile::AbstractString="info_contigs.txt",
+                    hclust_linkage::Symbol=:single)
+
+   contiginfo = readdlm(contiginfofile, '\t', header=true)
+   names = @view contiginfo[1,][:,1]
+   dist = builddist(contiginfofile,bg2file)
+   clustres = hclust(dist, linkage=hclust_linkage, branchorder=:optimal)
    neworder = ordernames(clustres.order,names)
    frinfo = reorient(contiginfofile,bg2file,neworder)
    write_reorient(genomeinfile,genomeoutfile,frinfo,genomefaifile)  
